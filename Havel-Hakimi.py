f08df36c1277b79693c8224e87dfad493c908092
ThanosPapas <55376797+ThanosPapas@users.noreply.github.com>
2021-03-05 00:24:57 +0200

    Add files via upload

---
+++ Havel-Hakimi.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import choice
+
+def test(lst, vathmos):
+    G = nx.Graph()
+    for i in range(len(lst)):
+        x = choice(lst)
+        while vathmos[x] == 0:
+            x = choice(lst)
+        tmp = vathmos[:]       #copy by value
+        tmp[x] = 0           #για να μην συνδεθεί ο κόμβος με τον εαυτό του
+        for j in range(vathmos[x]):
+            ind = tmp.index(max(tmp))
+            G.add_edge(x, ind)
+            tmp[ind] -=1
+            vathmos[ind] = tmp[ind] #αντιγράφω την αλλαγή στο original array
+            tmp[ind] = 0       #για να μην συνδεθεί ο κόμβος με έναν άλλο πάνω από μία φορά (με παίδεψε πολύ αυτό!)
+        vathmos[x] = 0
+        if all(v==0 for v in vathmos):
+            nx.draw_networkx(G)
+            break
+
+def insert(n):
+    lst = [i for i in range(n)]
+    vathmos =[]
+    for i in range(n):
+        x = int(input(f"Εισάγετε αριθμό δεσμών για τον {i + 1}ο κόμβο: "))
+        vathmos.append(x)
+    test(lst, vathmos)
+
+while True:
+    x = input("Εισάγετε αριθμό κόμβων: ")
+    try:
+        n = int(x)
+        if n <=1:
+            print("Ο αριθμός πρέπει να είναι μεγαλύτερος του 1. ", end='')
+            continue
+        insert(n)
+        plt.show()
+        break
+    except ValueError:
+        print("Απαιτείται ακέραιος αριθμός. ", end='')
